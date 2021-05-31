217b1aadcab2b8b010d79a3e1033a51c303a07d6
cgerasimovitch <c.gerasimovitch@gmail.com>
2021-05-31 19:26:47 +0300

    Reimport Navigation folder

---
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileHeaderView.swift
+//  Navigation
+//
+//  Created by Admin on 5/29/21.
+//  Copyright © 2021 Artem Novichkov. All rights reserved.
+//
+
+import UIKit
+class ProfileHeaderView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: UIScreen.main.bounds)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
---
+++ Navigation/Profile/ProfileViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ProfileViewController.swift
+//  Navigation
+//
+//  Created by Admin on 5/29/21.
+//  Copyright © 2021 Artem Novichkov. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    var profileView = ProfileHeaderView() as UIView
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .lightGray
+        edgesForExtendedLayout = []
+        view.addSubview(profileView)
+    }
+    
+    override func viewWillLayoutSubviews(){
+        profileView.frame = view.frame
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
